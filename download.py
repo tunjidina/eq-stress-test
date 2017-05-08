b0abc70993ce1a041c92dce899a966e262871da2
Mert Karsiyakalilar <mert.karsiyakalilar@gmail.com>
2017-05-07 20:11:15 -0400

    minor fixes

--- download.py
+++ download.py
@@ -8,7 +8,7 @@ from dateutil.relativedelta import relativedelta
 
 from db_utils import get_engine
 from db_utils import get_temptable, drop_temp_table,\
-                     insert_temp_price_table
+                     insert_temp_price_table, read_select
 
 TODAY = datetime.today()
 TEN_YRS = relativedelta(years=10)
@@ -23,6 +23,7 @@ def download_yahoo_prices(ticker, start_date=TEN_YRS_AGO, end_date=TODAY):
     :param end_date: Historical price end date
     :return: Price df or None
     """
+    print("Downloading prices for {}".format(ticker))
     try:
         prices = pdr.get_data_yahoo(symbols=ticker,
                                     start=start_date,
@@ -30,6 +31,7 @@ def download_yahoo_prices(ticker, start_date=TEN_YRS_AGO, end_date=TODAY):
     except:
         print("Error occured downloading prices for {}".format(ticker))
         return None
+    prices = prices.reset_index()
     prices.loc[:, "price_date"] = prices["Date"]
     prices.loc[:, "price"] = prices["Adj Close"]
     prices.loc[:, "ticker"] = ticker
@@ -37,12 +39,12 @@ def download_yahoo_prices(ticker, start_date=TEN_YRS_AGO, end_date=TODAY):
     return prices
 
 
-def insert_prices_to_db(db_engine, price_df, ticker):
+def insert_prices_to_db(db, price_df, ticker):
     """
     Insert into temp db
     Insert into price db
     Cleanup temp table
-    :param db_engine: SQLAlchemy engine 
+    :param db: SQLAlchemy engine 
     :param price_df: 
     :param ticker: str
     :param tablename: str
@@ -52,13 +54,23 @@ def insert_prices_to_db(db_engine, price_df, ticker):
     price_db_cols = ["price_date", "ticker", "source", "price"]
     df_to_insert = price_df[price_db_cols]
     try:
-        df_to_insert.to_sql(temptbl, db_engine)
+        df_to_insert.to_sql(temptbl, db)
+        insert_temp_price_table(db, temptbl, "eq_prices", debug=True)
     except:
-        print("Error loading in prices for {}".format(ticker))
+        print("Error loading in priceds for {}".format(ticker))
 
-    insert_temp_price_table(db, temptbl, "eq_prices", debug=True)
     drop_temp_table(db, temptbl, debug=True)
 
+
+def prices_exist(ticker, db):
+    q = """
+        SELECT *
+        FROM eq_prices
+        WHERE ticker = {_t}
+    """
+    p = {"_t": ticker}
+    res = read_select(db, q, p)
+    return res.shape[0] > 0
 
 def load_prices(db, tickers):
     """
@@ -68,5 +80,10 @@ def load_prices(db, tickers):
     :return: 
     """
     for ticker in tickers:
-        cur_prices = download_yahoo_prices(ticker)
-        insert_prices_to_db(db, cur_prices, ticker)
+        # _ TODO: check prices for ticker and get new range if necessary
+        if prices_exist(ticker, db):
+            print("prices already exist for {}".format(ticker))
+            continue
+        else:
+            cur_prices = download_yahoo_prices(ticker)
+            insert_prices_to_db(db, cur_prices, ticker)

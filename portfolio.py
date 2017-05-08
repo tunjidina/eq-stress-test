e63260b17499a73cc66f1f2b05eb39e08579a888
Mert Karsiyakalilar <mert.karsiyakalilar@gmail.com>
2017-05-07 20:11:50 -0400

    fix: Fixing Benchmark calcs

--- portfolio.py
+++ portfolio.py
@@ -6,47 +6,61 @@ Long/Short Equity Portfolio
 """
 
 import pandas as pd
-from db_utils import get_postgres_engine
+from db_utils import get_postgres_engine, get_temptable, drop_temp_table, insert_weights
 from download import load_prices
 from attribution import calc_daily_constituent_returns,\
                         calc_daily_portfolio_returns
-from risk import stress_test
+from weights import insert_weights
+
 
 class Benchmark:
-    def __init__(self, ticker):
-        self.ticker
+    def __init__(self, ticker="^GSPC"):
+        self.ticker = ticker
+        self.db = get_postgres_engine()
         self.load()
 
     def load(self):
-        load_prices(self.ticker)
-        calc_daily_constituent_returns(self.ticker)
+        load_prices(self.db, [self.ticker])
+        calc_daily_constituent_returns([self.ticker], self.db)
 
 
 class Portfolio:
     """
     """
-    def __init__(self, portfolio_name, benchmark_name, weights_path):
+    def __init__(self, portfolio_name, benchmark_name="^GSPC", weights_path="./weights.csv"):
         self.db = get_postgres_engine()
         self.weights_path = weights_path
+        self.portfolio_name = portfolio_name
         self.benchmark = Benchmark(benchmark_name)
 
     def load_weights(self):
+        # _ load weights from csv
         weights = pd.read_csv(self.weights_path)
+        weights.loc[:, "portfolio_name"] = self.portfolio_name
+
+        # _ load weights into db
+        tmptbl = get_temptable()
+        weights.to_sql(tmptbl, self.db)
+        insert_weights(self.db, tmptbl, self.portfolio_name)
+        drop_temp_table(tmptbl)
+
+        # _ store weights on class for convenience
         self.weights = weights
         self.constituents = weights.ticker.tolist()
 
     def load_prices(self):
+        # _ load prices into db
         load_prices(self.constituents)
 
     def calc_returns(self):
-        self.constituent_returns = calc_daily_constituent_returns(self.constituents)
+        self.constituent_returns = calc_daily_constituent_returns(self.constituents, self.db)
         self.portfolio_returns = calc_daily_portfolio_returns(self.constituents, self.weights)
 
     def calc_portfolio_beta(self):
         pass
 
     def generate_report(self):
-        stress_test()
+        pass
 
     def run(self):
         self.load_weights()

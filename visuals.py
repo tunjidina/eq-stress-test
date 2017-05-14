a297160329ffc11155a16883cee4dc052e32a32b
Mert Karsiyakalilar <mert.karsiyakalilar@gmail.com>
2017-05-13 23:17:46 -0400

    data visualations

--- visuals.py
+++ visuals.py
@@ -1,26 +1,30 @@
+import os
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 
 def pos_breakdown(weight_df, path="./images/pos_breakdown.png"):
     f, (ax1, ax2) = plt.subplots(1, 2)
-    weight_df[weight_df.weights > 0]["weights"].sort_values()\
-                                               .plot(kind="pie",
-                                                     figsize=(35, 17.5),
-                                                     autopct='%.2f',
-                                                     ax=ax1,
-                                                     colors=sns.light_palette("green"),
-                                                     fontsize=20)
+    weight_df[weight_df.weight > 0]["weight"].sort_values() \
+        .plot(kind="pie",
+              figsize=(35, 17.5),
+              autopct='%.2f',
+              ax=ax1,
+              colors=sns.color_palette("Blues"),
+              fontsize=20)
     ax1.set_title("Longs", fontsize=40)
-    (weight_df[weight_df.weights < 0] * -1)["weights"].sort_values()\
-                                                      .plot(kind="pie",
-                                                            figsize=(35, 17.5),
-                                                            autopct='(%.2f)', ax=ax2,
-                                                            colors=sns.color_palette("green"),
-                                                            fontsize=20)
+    (weight_df[weight_df.weight < 0] * -1)["weight"].sort_values() \
+        .plot(kind="pie",
+              figsize=(35, 17.5),
+              autopct='(%.2f)', ax=ax2,
+              colors=sns.color_palette("Blues"),
+              fontsize=20)
     ax2.set_title("Shorts", fontsize=40)
     f.savefig(path)
-    return path
+
+    # _ send only the filename
+    filename = os.path.basename(path)
+    return filename
 
 
 def cumulative_returns(agg_df, path="./images/cum_returns.png"):
@@ -33,7 +37,10 @@ def cumulative_returns(agg_df, path="./images/cum_returns.png"):
     ax.set_ylabel("Returns")
 
     f.savefig(path)
-    return path
+
+    # _ send only the filename
+    filename = os.path.basename(path)
+    return filename
 
 
 def scenario_returns(scenario_returns_df, pf_beta, bench_name, path="./images/scenario_returns.png"):
@@ -43,17 +50,20 @@ def scenario_returns(scenario_returns_df, pf_beta, bench_name, path="./images/sc
     ax.set_xlabel("Scenarios (pct up/down)")
     ax.set_ylabel("Returns")
     f.savefig(path)
-    return path
+
+    # _ send only the filename
+    filename = os.path.basename(path)
+    return filename
 
 
-def correlation_chart(rolling_correlations, path="./images/rolling_corr.png"):
-    pf_daily_ret = p.portfolio_returns.dropna().ffill()
-    bench_daily_ret = p.benchmark_returns['2016-11-16':]
-    cor_df = DataFrame({"portfolio": pf_daily_ret, "SP500": bench_daily_ret}).ffill()
-    cor_df.rolling(window=30).corr(cor_df["portfolio"], cor_df["SP500"])
+def correlation_chart(cor_df, path="./images/rolling_corr.png"):
     f, ax = plt.subplots()
-    pd.rolling_corr(cor_df["portfolio"], cor_df["SP500"], window=30).dropna().plot(ax=ax, figsize=(20, 10))
+    cor_df.plot(ax=ax, figsize=(20, 10))
     ax.set_title("30-day rolling correlation portfolio vs SP500")
     ax.set_ylabel("Correlation")
-    return path
+    f.savefig(path)
+
+    # _ send only the filename
+    filename = os.path.basename(path)
+    return filename
 

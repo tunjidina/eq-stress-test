520c58e0ba98bb4a6400b9d876711e4ad9587101
Mert Karsiyakalilar <mert.karsiyakalilar@gmail.com>
2017-05-14 10:38:09 -0400

    adding override args

--- run_stress_test.py
+++ run_stress_test.py
@@ -10,24 +10,26 @@ def parse_args():
     parser = ArgumentParser()
     parser.add_argument("--portfolio_name", default="TEST_PF")
     parser.add_argument("--weights_file", default="./weights.csv")
-    parser.add_argument("--report_template_path", default="./template/report_template.html")
-    parser.add_argument("--report_target_path", default="./report/report.pdf")
+    parser.add_argument("--report_template_name", default="stress_test_rpt.html")
+    parser.add_argument("--report_target_path", default="./reports/targets/report.html")
+    parser.add_argument("--override", action="store_true")
     args = parser.parse_args()
     return args
 
 def run_stress_test():
+
     args = parse_args()
 
     weights_path = args.weights_file
     portfolio_name = args.portfolio_name
 
-    pf = Portfolio(portfolio_name, weights_path)
+    pf = Portfolio(portfolio_name, weights_path=weights_path)
 
     pf.load_weights()
     pf.download_prices()
     pf.calc_returns()
 
-    template_path = args.template_path
+    template_path = args.report_template_name
     target_path = args.report_target_path
 
     rpt = StressTest(pf, template_path, target_path)
